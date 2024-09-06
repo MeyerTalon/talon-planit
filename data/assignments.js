411669adeb0e5792476d3f0a7778238d299d047e
planit <planit@tanjeffreyz.github.io>
2024-09-06 15:35:11 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -22,6 +22,13 @@ assignments = {
       "submitted": true
     },
     {
+      "title": "Homework 2",
+      "course": "CS 61A",
+      "dueDate": "2024-09-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/843175",
+      "submitted": false
+    },
+    {
       "title": "Hog Checkpoint",
       "course": "CS 61A",
       "dueDate": "2024-09-12T23:59:00",
