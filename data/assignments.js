137251897381229e9325af007d663bf489ca9f3b
planit <planit@tanjeffreyz.github.io>
2024-10-26 02:58:32 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -556,8 +556,8 @@ assignments = {
       "title": "HW8",
       "course": "EECS 16A",
       "dueDate": "2024-10-27T23:59:00",
-      "link": "https://www.gradescope.com/courses/823368",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/823368/assignments/5189076/submissions/282291416",
+      "submitted": true
     }
   ]
 };
