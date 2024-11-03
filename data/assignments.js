6d69d85c763f03db760f6bc88bb16c8607bba53e
planit <planit@tanjeffreyz.github.io>
2024-11-03 10:39:14 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -591,8 +591,8 @@ assignments = {
       "title": "HW9",
       "course": "EECS 16A",
       "dueDate": "2024-11-03T23:59:00",
-      "link": "https://www.gradescope.com/courses/823368",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/823368/assignments/5224821/submissions/284971425",
+      "submitted": true
     },
     {
       "title": "Exam 2 Redo",
