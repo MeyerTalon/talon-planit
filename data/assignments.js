d4570ae0bbafba3408a4544287d4a7221808515a
planit <planit@tanjeffreyz.github.io>
2024-11-22 02:07:37 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -659,14 +659,21 @@ assignments = {
       "title": "Mini-Vitamin 12B",
       "course": "CS 70",
       "dueDate": "2024-11-21T19:30:00",
-      "link": "https://www.gradescope.com/courses/836005/assignments/5326045/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/836005/assignments/5326045/submissions/291680091",
+      "submitted": true
     },
     {
       "title": "Homework 12",
       "course": "CS 70",
       "dueDate": "2024-11-23T16:00:00",
       "link": "https://www.gradescope.com/courses/836005",
+      "submitted": false
+    },
+    {
+      "title": "Mini-Vitamin 13A",
+      "course": "CS 70",
+      "dueDate": "2024-11-26T18:00:00",
+      "link": "https://www.gradescope.com/courses/836005/assignments/5352068/submissions/new",
       "submitted": false
     }
   ],
