e4124c65be4e04a31c472a29ab660af240f94e75
planit <planit@tanjeffreyz.github.io>
2025-02-01 08:36:24 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -34,6 +34,13 @@ assignments = {
       "dueDate": "2025-01-29T23:59:00",
       "link": "https://www.gradescope.com/courses/949051/assignments/5576303/submissions/303358243",
       "submitted": true
+    },
+    {
+      "title": "HW2 Write-Up",
+      "course": "CS 189/289A",
+      "dueDate": "2025-02-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/949051",
+      "submitted": false
     }
   ],
   "EECS 16B ": [
