e9685be91cf70406781947083031c1e5fb3fa117
planit <planit@tanjeffreyz.github.io>
2025-02-03 16:41:05 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -57,6 +57,13 @@ assignments = {
       "dueDate": "2025-02-07T17:00:00",
       "link": "https://www.gradescope.com/courses/963410",
       "submitted": false
+    },
+    {
+      "title": "Lab1 Result Sheet",
+      "course": "EECS 16B ",
+      "dueDate": "2025-02-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/963410",
+      "submitted": false
     }
   ]
 };
