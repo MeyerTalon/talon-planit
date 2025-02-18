881cd81fe46e129812269f4228c4d6a0e06d851c
planit <planit@tanjeffreyz.github.io>
2025-02-18 07:33:09 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -89,7 +89,7 @@ assignments = {
     {
       "title": "HW1_selfgrading",
       "course": "EECS 16B ",
-      "dueDate": "2025-02-18T17:00:00",
+      "dueDate": "2025-02-21T17:00:00",
       "link": "https://www.gradescope.com/courses/963410/assignments/5781654/submissions/new",
       "submitted": false
     },
@@ -105,6 +105,13 @@ assignments = {
       "course": "EECS 16B ",
       "dueDate": "2025-02-22T23:59:00",
       "link": "https://www.gradescope.com/courses/963410",
+      "submitted": false
+    },
+    {
+      "title": "HW2_selfgrading",
+      "course": "EECS 16B ",
+      "dueDate": "2025-02-23T17:00:00",
+      "link": "https://www.gradescope.com/courses/963410/assignments/5807465/submissions/new",
       "submitted": false
     }
   ]
